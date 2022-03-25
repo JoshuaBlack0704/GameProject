bdaacfd6fd52286985f93ad990e8a9182c8216c8
josh <knightrider0704@gmail.com>
2022-03-25 02:08:07 -0500

    Trying out externs

--- Game/Source/main.cpp
+++ Game/Source/main.cpp
@@ -2,8 +2,10 @@
 // Created by josh on 3/23/22.
 //
 #include <VkLayer.hpp>
+#include <spdlog/spdlog.h>
 
 int main(){
-    VulkanInitalizer initalizer;
-    initalizer.Init();
+    auto h = [](GLFWwindow*, int, int){spdlog::info("Resizing");};
+    vkl::AddResizeCallBack(h);
+    vkl::InitVkLayer();
 }
---
+++ VulkanLayer/Include/VkLayer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+#include <functional>
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include "../VkBootstrap/VkBootstrap.h"
+
+#define VK_LAYER_NAMESPACE vkl
+
+namespace VK_LAYER_NAMESPACE
+{
+    //Static variables where only one should be in existence at a time
+    extern GLFWwindow* window = nullptr;
+    extern vkb::Instance instance = vkb::Instance();
+    extern vkb::PhysicalDevice pDevice;
+    extern vkb::Device lDevice;
+    extern vkb::Swapchain swapchain;
+    extern VkSurfaceKHR surface;
+    extern std::vector<std::function<void(GLFWwindow* window, int width, int height)>> resizeCallbacks;
+
+    //Static settings
+    extern int windowWidth;
+    extern int windowHeight;
+    extern bool useImGui;
+
+
+    ///This function contains arguments for all the variable settings
+    void SetSettings(int windowWidth, int windowHeight, bool useImGui, bool resizable);
+    ///This creates all of the top level vulkan objects as well as calls whatever functions are
+    ///In the resizeCallbacks vector
+    void InitVkLayer();
+    void AddResizeCallBack(std::function<void(GLFWwindow *window, int width, int height)> callback);
+
+
+}
+
